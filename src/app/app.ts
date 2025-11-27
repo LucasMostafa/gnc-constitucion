83db3294ab9d8af57df3e9f61ad0908a547405b6
Lucas Mostafa <mostafalucas@gmail.com>
2025-11-27 00:15:28 -0300

    dassda

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { Navbar } from './shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterLink, CommonModule, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
