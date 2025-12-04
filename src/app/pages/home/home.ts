5f2a156b695c50463981aff6fc37ea48d41c8308
Lucas Mostafa <mostafalucas@gmail.com>
2025-12-04 01:48:26 -0300

    Front de Servicios y turnos, algunas visuales cargadas

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,13 @@
 // src/app/pages/home/home.component.ts
 import { Component, OnInit } from '@angular/core';
+import { Servicios } from "../servicios/servicios";
+import { Turnos } from "../turnos/turnos";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
-  styleUrls: ['./home.scss']
+  styleUrls: ['./home.scss'],
+  imports: [Servicios, Turnos]
 })
 export class Home implements OnInit {
   constructor() { }
